ac1edfbd52bda6f0dc42d2146c8a6043ad55a32c
Ashwin <mahesh.ashwin1998@gmail.com>
2018-07-03 19:01:26 -0700

    working on slider

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number: UILabel!
+    
+    @IBOutlet weak var label_left: UILabel!
+    @IBOutlet weak var label_middle: UILabel!
+    @IBOutlet weak var label_right: UILabel!
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        label_middle.text=String(100*sender.value)+"%"
+    }
+    @IBAction func resetPushed(_ sender: UIButton) {
+        number.text=""
+    }
+    @IBAction func numberPressed(_ sender: UIButton) {
+        var old:String=number.text!
+        let toAdd:String=sender.title(for: .normal)!
+        var allowDot=true
+        if old.count>=10 || old=="OVERLOAD"{
+            number.text="OVERLOAD"
+            return
+        }
+        if sender.title(for: .normal)=="."{
+            for char in old{
+                if char=="."{
+                    allowDot=false
+                    break;
+                }
+            }
+            if allowDot==true{
+                old.append(toAdd)
+                number.text=old
+            }
+        }
+        else{
+            old.append(toAdd)
+            number.text=old
+        }
+       
+//        number.text.append(sender.title(for: .normal))
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        number.text=""
     }
 
     override func didReceiveMemoryWarning() {
